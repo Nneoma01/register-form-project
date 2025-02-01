5434d3dee6897eedd72f54848fe46134c686fafe
Nneoma01 <toughspirit>
2025-02-01 17:57:00 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HeaderSection from './components/header';
+import InfoSection from './components/info';
+
+function App() {
+  return (
+    <div className="App-body">
+      <HeaderSection />
+      <InfoSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/info.js
@@ -0,0 +1,36 @@
+import "../components/info.css";
+
+
+const InfoSection = () => {
+  return (
+    <section className="info-section">
+      <div className="empty"></div>
+      <div className="info-text">
+        <div className="info-title">DESIGN YOUR LIFE 1.0</div>
+        <div className="info">STOP Living on Autopilot , START Designing Your Dream Life. Join this exclusive 2-hour matsrerclass and discover the proven framework that is helping professionals transform chaos into clarity. Plus,  Get instant access to our Life Audit Toolkit ($75 value) completely free.</div>
+      </div>
+    </section>
+  )
+}
+
+
+{/* const InfoSection = () => {
+    return (
+     <section>
+        <div className="empty"></div>
+          <div>
+            <div>DESIGN YOUR LIFE 1.0</div>
+            <div>STOP Living on Autopilot , START Designing Your Dream Life.</br>Join this exclusive 2-hour matsrerclass 
+              and discover the proven framework that is helping professionals transform chaos into clarity. Plus, 
+             Get instant access to our Life Audit Toolkit ($75 value) completely free
+            </div>
+          </div>  
+        </div>
+      </section>
+          
+
+      
+    );
+  } */}
+
+export default InfoSection;
